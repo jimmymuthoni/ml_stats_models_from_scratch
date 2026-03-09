b381b369940f97942bfb2a95a64688cdc66d7ea0
jimmymuthoni <jimmymuthoni26@gmail.com>
2026-03-09 17:15:23 +0300

    regression performance metrics

--- utils/performance_metrics.py
+++ utils/performance_metrics.py
@@ -1,19 +1,22 @@
 import numpy as np
 
-#mse -----> mean squared error
+#mse -----> mean squared error = (sum(y-y^)**2) / n n---> number of data points
 def mse(y_true, y_pred):
     return np.mean((y_true - y_pred) ** 2)
     
 
-#rmse(root mean squared error)
+#rmse(root mean squared error = sqr((sum(y-y^)**2) / n)
 def rmse(y_true, y_pred):
     mse = np.mean((y_true - y_pred) ** 2)
     return np.sqrt(mse)
 
-#r2 score
+#r2 score ----> 1 - (ss_residual/ss_total)
+#ss_residual ---> sum of square residual, ss_total ----> sum of square total
 def r2_score(y_true, y_pred):
     ss_residual = np.sum((y_true - y_pred) ** 2)
     y_mean = np.mean(y_true)
     ss_total = np.sum((y_true - y_mean) ** 2)
     r2 = 1 - (ss_residual / ss_total)
     return r2
+
+    
